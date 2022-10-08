4ca8428c2539c047dc9bd8be0e8d19bdb7d4743d
github peterdo415 <btsmanne@gmail.com>
2022-10-08 14:38:14 +0900

    管理機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   resources :questions
+
+  namespace :admin do
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    resources :users, only: [:index, :destroy]
+    resources :questions, only: [:index, :destroy]
+  end
 end
