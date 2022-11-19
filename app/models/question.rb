e0c543882133a52f97043957f790dafcfdb704f1
tanamaru <yt.0408.mmm@gmail.com>
2022-11-19 23:58:05 +0900

    回答機能

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
-
+  has_many :answers, dependent: :destroy
   validates :title, presence: true, length: {minimum:1, maximum:30}
   validates :body, presence: true
 end
